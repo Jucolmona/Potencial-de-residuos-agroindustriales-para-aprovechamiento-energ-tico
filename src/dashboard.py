222d435a2981297a9c7e718d0a36be3ab532be53
Juan Camilo Moná Luján <84756210+Jucolmona@users.noreply.github.com>
2025-08-04 21:13:11 +0000

    ...

---
+++ src/dashboard.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def encabezado_dashboard():
+    st.header('Potencial de residuos agroindustriales para aprovechamiento energético')
+    
---
+++ src/main.py
@@ -0,0 +1,4 @@
+import dashboard as dsb
+
+if __name__ == "__main__":
+    dsb.encabezado_dashboard()
